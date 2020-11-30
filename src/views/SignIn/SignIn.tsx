8549b529ddbf5f8566be36dc408a120d4d389ed0
OxyTom <oxytom8@gmail.com>
2020-11-30 15:28:14 +0100

    started signup form

--- src/views/SignIn/SignIn.tsx
+++ src/views/SignIn/SignIn.tsx
@@ -8,18 +8,20 @@ import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 export function SignIn(): JSX.Element {
     return (
-        <form className="sign-app">
-            <div className="sign-textbox">
-                <FontAwesomeIcon icon={faUser} />
-                <input type="text" id="username" name="username" placeholder="Nom d'utilisateur" />
-                <label htmlFor="username">Username</label>
-            </div>
-            <div className="sign-textbox">
-                <FontAwesomeIcon icon={faLock} />
-                <input type="password" id="passwd" name="passwd" placeholder="Mot de passe" />
-                <label htmlFor="passwd">Password</label>
-            </div>
-            <input type="submit" value="Connexion" />
-        </form>
+        <div className="App">
+            <form className="signin-app">
+                <div className="sign-textbox">
+                    <FontAwesomeIcon icon={faUser} />
+                    <input type="text" id="username" name="username" placeholder="Nom d'utilisateur" />
+                    <label htmlFor="username">Username</label>
+                </div>
+                <div className="sign-textbox">
+                    <FontAwesomeIcon icon={faLock} />
+                    <input type="password" id="passwd" name="passwd" placeholder="Mot de passe" />
+                    <label htmlFor="passwd">Password</label>
+                </div>
+                <input type="submit" value="Connexion" />
+            </form>
+        </div>
     );
 }
--- src/views/SignUp/SignUp.tsx
+++ src/views/SignUp/SignUp.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 
+import './SignUp.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+
 export function SignUp(): JSX.Element {
     return (
         <div className="App">
+            <form className="signup-app">
+                <div className="sign-textbox">
+                    <FontAwesomeIcon icon={faUser} />
+                    <input type="text" id="username" name="username" placeholder="Nom d'utilisateur" />
+                    <label htmlFor="username">Username</label>
+                </div>
 
+                <div className="sign-spacer"></div>
+
+                <div className="sign-textbox">
+                    <FontAwesomeIcon icon={faUser} />
+                    <input type="email" id="email" name="email" placeholder="Addresse mail" />
+                    <label htmlFor="email">Email</label>
+                </div>
+
+                <div className="sign-spacer"></div>
+
+                <div className="sign-textbox">
+                    <FontAwesomeIcon icon={faLock} />
+                    <input type="password" id="passwd" name="passwd" placeholder="Mot de passe" />
+                    <label htmlFor="passwd">Password</label>
+                </div>
+                <div className="sign-textbox">
+                    <FontAwesomeIcon icon={faLock} />
+                    <input type="password" id="passwdconf" name="passwdconf" placeholder="Mot de passe" />
+                    <label htmlFor="passwdconf">Password</label>
+                </div>
+
+                <input type="submit" value="Inscription" />
+            </form>
         </div>
     );
 }
