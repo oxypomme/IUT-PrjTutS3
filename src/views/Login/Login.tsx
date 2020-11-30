eb7da8d8e7e7c4017ef4815a670d350b351e7f85
OxyTom <oxytom8@gmail.com>
2020-11-30 16:44:39 +0100

    Added labels for modules

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -8,8 +8,14 @@ import { SignUp } from '../../features/signup/SignUp';
 export function Login(): JSX.Element {
     return (
         <div className="App login">
-            <SignIn />
-            <SignUp />
+            <div>
+                <h2>Connexion</h2>
+                <SignIn />
+            </div>
+            <div>
+                <h2>Inscription</h2>
+                <SignUp />
+            </div>
         </div>
     );
 }
