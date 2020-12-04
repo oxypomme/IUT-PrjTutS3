337dbabce03503c7890fca89097cb8ffe1cc4b97
OxyTom <oxytom8@gmail.com>
2020-12-04 17:27:15 +0100

    readded passwd conf + removed name

--- src/features/accounts/signup/SignUp.tsx
+++ src/features/accounts/signup/SignUp.tsx
@@ -12,25 +12,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 function onSubmit(): User | null {
-    const username = document.getElementById("r-username") as HTMLInputElement | null;
     const mail = document.getElementById("r-email") as HTMLInputElement | null;
     const passwd = document.getElementById("r-passwd") as HTMLInputElement | null;
+    const passwdconf = document.getElementById("r-passwdconf") as HTMLInputElement | null;
 
     // Check if elements are found
-    if (username && mail && passwd)
+    if (mail && passwd && passwdconf)
         // Check if values are not null or empty
-        // TODO: password strength bar
-        // TODO: check if email is really an email
-        if ((typeof username.value != 'undefined' && username.value)
-            && (typeof mail.value != 'undefined' && mail.value)
-            && (typeof passwd.value != 'undefined' && passwd.value)) {
-            // Reset form
-            username.value = "";
-            mail.value = "";
-            passwd.value = "";
+        if ((typeof mail.value != 'undefined' && mail.value)
+            && (typeof passwd.value != 'undefined' && passwd.value)
+            && (typeof passwdconf.value != 'undefined' && passwdconf.value))
+            // TODO: password strength bar
+            // TODO: check if email is really an email
+            // Check if the password confirmation is true
+            if (passwd?.value === passwdconf?.value) {
+                // Reset form
+                mail.value = "";
+                passwd.value = "";
+                passwdconf.value = "";
 
-            return new User(username.value, mail.value, passwd.value);
-        } else
+                return new User("username", mail.value, passwd.value);
+            } else
+                alert("Les mots de passes ne correspondent pas.");
+        else
             alert("Au moins un champ est nul.");
     else
         alert("Un des champs n'a pas été trouvé.");
@@ -42,23 +46,6 @@ export function SignUp(): JSX.Element {
 
     return (
         <div>
-            <div className={styles.textbox}>
-                <FontAwesomeIcon icon={faUser} />
-                <input
-                    type="text"
-                    id="r-username"
-                    name="username"
-                    placeholder="Nom d'utilisateur"
-                />
-                <label
-                    className={styles.label}
-                    htmlFor="username">
-                    Username
-                </label>
-            </div>
-
-            <div className={styles.spacer}></div>
-
             <div className={styles.textbox}>
                 <FontAwesomeIcon icon={faUser} />
                 <input
@@ -81,6 +68,23 @@ export function SignUp(): JSX.Element {
                     placeholder="Mot de passe"
                 />
                 <label className={styles.label} htmlFor="passwd">Password</label>
+            </div>
+            <div className={styles.textbox}>
+                <FontAwesomeIcon icon={faLock} />
+                <input
+                    type="password"
+                    id="r-passwdconf"
+                    name="passwdconf"
+                    placeholder="Mot de passe"
+                    onChange={event => {
+                        const passwd = document.getElementById("r-passwd") as HTMLInputElement;
+                        if (passwd && passwd.value != event.target.value)
+                            event.target.parentElement?.classList.add(styles.errorfield);
+                        else
+                            event.target.parentElement?.classList.remove(styles.errorfield);
+                    }}
+                />
+                <label className={styles.label} htmlFor="passwdconf">Password</label>
             </div>
 
             <button
