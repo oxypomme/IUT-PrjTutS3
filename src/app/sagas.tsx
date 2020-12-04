1b181570d5b9e9abb3938664f0afc78e9b73e82a
OxyTom <oxytom8@gmail.com>
2020-12-04 19:11:31 +0100

    Sagas will now use firebase, and that's beautifull

--- src/app/sagas.tsx
+++ src/app/sagas.tsx
@@ -1,4 +1,12 @@
+import firebase from 'firebase';
+import ReduxSagaFirebase from 'redux-saga-firebase';
+import { firebaseConfig } from './firebaseconfig';
+
+const reduxSagaFirebase = new ReduxSagaFirebase(firebase.initializeApp(firebaseConfig));
+
 function* mySaga() {
+    // https://redux-saga.js.org/
+    // https://www.npmjs.com/package/redux-saga-firebase
     yield "";
 }
 
