e6f929f819c62f6ca24963ac30452dae1ea63c33
AvaN0x <clement.ricatte57@gmail.com>
2020-12-05 16:23:12 +0100

    now using react webcam to do the exact same thing as before

--- src/features/camera/Camera.tsx
+++ src/features/camera/Camera.tsx
@@ -1,55 +1,27 @@
 import React from "react";
-// import { useDispatch } from 'react-redux';
 import styles from "./Camera.module.css";
+import Webcam from "react-webcam";
 
-export function Camera({ test = 12 }): JSX.Element {
-  const video = React.useRef<HTMLVideoElement>();
-  const canvas = React.useRef<HTMLCanvasElement>();
-
+export function Camera(): JSX.Element {
+  const webcamRef = React.useRef<Webcam>();
   const [images, setImages] = React.useState<Array<string>>([]);
 
-  React.useEffect(() => {
-    const initStream = async () => {
-      const stream = await navigator?.mediaDevices?.getUserMedia({
-        video: true,
-      });
-      if (stream && video) {
-        video.current.srcObject = stream;
-        video.current.play();
-      }
-    };
-    initStream();
-  }, []);
-
-  // const dispatch = useDispatch();
-  const snap = () => {
-    canvas.current.getContext("2d")?.drawImage(video.current, 0, 0, 640, 480);
-    setImages([...images, canvas.current.toDataURL()]);
-  };
+  const snap = React.useCallback(() => {
+    setImages([...images, webcamRef.current.getScreenshot()]);
+  }, [images, setImages]);
 
   return (
     <div>
-      <div className={styles.container}>
-        <video
-          ref={video}
-          width='640'
-          height='480'
-          className={styles.video}
-        ></video>
-        <button id='snap' onClick={snap}>
-          Snap Photo
-        </button>
-        <canvas
-          id='canvas'
-          ref={canvas}
-          width='640'
-          height='480'
-          className={styles.canvas}
-        ></canvas>
-        {images.map((image, index) => (
-          <img src={image} key={index} style={{ width: 320, height: 240 }} />
-        ))}
-      </div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        className={styles.video}
+      />
+      <button onClick={snap}>Snap</button>
+      {images.map((image, index) => (
+        <img src={image} key={index} style={{ width: 320 }} />
+      ))}
     </div>
   );
 }
