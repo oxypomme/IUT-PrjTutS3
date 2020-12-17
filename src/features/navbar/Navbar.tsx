41368f3b3b8b8435874ef453a3a5461ee959dad4
OxyTom <oxytom8@gmail.com>
2020-12-17 11:57:47 +0100

    added active to navbar

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../../logo.svg';
-import styles from './Navbar.module.css'
+
+import styled from '@emotion/styled';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getConnection, setConnected } from '../accounts/accountSlice';
+
+const NavBar = styled.nav``;
+const NavList = styled.ul`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    height: 49px;
+    z-index: 9999;
+`;
+const NavLogo = styled.img`
+    height: 32px;
+    padding: 8px 8px 0px 16px;
+`;
+const NavItem = styled.li<{ floatRight?: boolean }>`
+    float: ${props => props.floatRight ? 'right' : 'left'};
+
+    & > a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+    
+    & > a:hover {
+        background-color: #111;
+    }
+    
+    & > a.active {
+        background-color: #111;
+    }
+`;
 
 export const Navbar = (): JSX.Element => {
     const dispatch = useDispatch();
@@ -17,20 +55,32 @@ export const Navbar = (): JSX.Element => {
     }
 
     return (
-        <nav className={styles.navbar}>
-            <ul className={styles.navlist}>
-                <li><img src={logo} className={styles.navlogo} alt="logo" /></li>
-                <li><Link to="/">Accueil</Link></li>
+        <NavBar>
+            <NavList>
+                <NavItem>
+                    <NavLogo src={logo} alt="logo" />
+                </NavItem>
+                <NavItem>
+                    <NavLink exact to="/">Accueil</NavLink>
+                </NavItem>
                 {guser &&
-                    <li className={styles.navfloat}><Link to="/profile">Mon profil</Link></li>
+                    <NavItem floatRight={true}>
+                        <NavLink to="/profile">Mon profil</NavLink>
+                    </NavItem>
                 }
                 {guser ?
-                    <li className={styles.navfloat}><a href="#" onClick={handleLogout}>Déconnexion</a></li>
+                    <NavItem floatRight={true}>
+                        <a href="#" onClick={handleLogout}>Déconnexion</a>
+                    </NavItem>
                     :
-                    <li className={styles.navfloat}><Link to="/login">Connexion</Link></li>
+                    <NavItem floatRight={true}>
+                        <NavLink to="/login">Connexion</NavLink>
+                    </NavItem>
                 }
-                <li className={styles.navfloat}><Link to="/camera">Test caméra</Link></li>
-            </ul>
-        </nav >
+                <NavItem>
+                    <NavLink to="/camera">Test caméra</NavLink>
+                </NavItem>
+            </NavList>
+        </NavBar>
     );
 }
