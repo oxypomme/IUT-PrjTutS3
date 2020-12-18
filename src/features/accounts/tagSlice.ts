e598b93cc2dfba33d434e2dcdc4d9b77e9dff903
OxyTom <oxytom8@gmail.com>
2020-12-18 18:55:36 +0100

    added an example for mapping

--- src/features/accounts/tagSlice.ts
+++ src/features/accounts/tagSlice.ts
@@ -2,6 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface ITag { value: string, label: string; }
 
+const mapping = (array: string[]): ITag[] =>
+    array.map((label, key) => ({
+        value: key.toString(),
+        label
+    }));
+
+
 export const tagSlice = createSlice({
     name: 'tags',
     initialState: {
