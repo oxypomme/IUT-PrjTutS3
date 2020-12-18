1f3b5507e2374ac705af8a7e791d503d1d14fbc4
OxyTom <oxytom8@gmail.com>
2020-12-18 18:26:56 +0100

    started the creation of types

---
+++ src/app/sagas/types.ts
@@ -0,0 +1,10 @@
+const tags = {
+    FETCH_TAGS: 'FETCH_TAGS_REQUESTED',
+    FETCH_TAG: 'FETCH_TAG_REQUESTED',
+    SUCCESS: 'FETCH_TAGS_SUCCESS',
+    FAILURE: 'FETCH_TAGS_FAILURE'
+}
+
+export default {
+    tags: tags,
+}
