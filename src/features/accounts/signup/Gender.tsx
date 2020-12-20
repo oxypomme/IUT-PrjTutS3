72fd5041e17902ba43ad2ff50fba9e15457900ef
OxyTom <oxytom8@gmail.com>
2020-12-20 09:55:24 +0100

    Splitted Gender and Orientation

--- src/features/accounts/signup/Gender.tsx
+++ src/features/accounts/signup/Gender.tsx
@@ -9,15 +9,7 @@ const enum EGender {
     Men
 }
 
-const enum EOrientation {
-    Other = 0,
-    Bisexual,
-    Homosexual,
-    Heterosexual
-}
-
 export interface IGender { value: number, label: string; }
-export interface IOrientation { value: number, label: string; }
 
 export const Gender = (): JSX.Element => {
     const [selectedGender, setSelectedGender] = React.useState<Array<IGender>>();
@@ -39,27 +31,4 @@ export const Gender = (): JSX.Element => {
             />
         </form>
     );
-}
-export const Orientation = (): JSX.Element => {
-    const [selectedOrientation, setSelectedOrientation] = React.useState<Array<IGender>>();
-
-    const orientations = [
-        { value: EOrientation.Homosexual, label: 'Homosexuel' },
-        { value: EOrientation.Heterosexual, label: 'Hétérosexuel' },
-        { value: EOrientation.Bisexual, label: 'Bisexuel' },
-        { value: EOrientation.Other, label: 'Autre' }
-    ] as IOrientation[]
-
-    return (
-
-        <form>
-            <Select
-                isSearchable={true}
-                isClearable={true}
-                onChange={myorientation => setSelectedOrientation([myorientation as IOrientation])}
-                options={orientations}
-            />
-        </form>
-    );
-
 }
---
+++ src/features/accounts/signup/Orientation.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { useDispatch } from 'react-redux';
+import Select from "react-select";
+
+const enum EOrientation {
+    Other = 0,
+    Bisexual,
+    Homosexual,
+    Heterosexual
+}
+
+export interface IOrientation { value: number, label: string; }
+
+export const Orientation = (): JSX.Element => {
+    const [selectedOrientation, setSelectedOrientation] = React.useState<Array<IGender>>();
+
+    const orientations = [
+        { value: EOrientation.Homosexual, label: 'Homosexuel' },
+        { value: EOrientation.Heterosexual, label: 'Hétérosexuel' },
+        { value: EOrientation.Bisexual, label: 'Bisexuel' },
+        { value: EOrientation.Other, label: 'Autre' }
+    ] as IOrientation[]
+
+    return (
+        <form>
+            <Select
+                isSearchable={true}
+                isClearable={true}
+                onChange={myorientation => setSelectedOrientation([myorientation as IOrientation])}
+                options={orientations}
+            />
+        </form>
+    );
+
+}
