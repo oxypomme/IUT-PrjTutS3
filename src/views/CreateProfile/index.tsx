27f32e5e683a088df1277ec09eb4f894e299c86c
OxyTom <oxytom8@gmail.com>
2020-12-20 19:13:25 +0100

    why it didn't commit ?

--- src/views/CreateProfile/index.tsx
+++ src/views/CreateProfile/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import RegisterPersonal from '../../features/accounts/signup/RegisterPersonal';
 
-export function CreateProfile(): JSX.Element {
+import { Gender } from "../../features/accounts/signup/Gender";
+import { Orientation } from "../../features/accounts/signup/Orientation";
+import { Tags } from "../../features/accounts/signup/Tags";
+
+export function CreatePersonal(): JSX.Element {
     return (
         <div className='App'>
             <div>
                 <h2>Formulaire</h2>
                 <RegisterPersonal />
+            </div>
+        </div>
+    );
+}
+
+export function CreatePreferences(): JSX.Element {
+    return (
+        <div className='App'>
+            <div>
+                <h2>Formulaire</h2>
+                <Gender />
+                <Orientation />
+                <Tags />
             </div>
         </div>
     );
