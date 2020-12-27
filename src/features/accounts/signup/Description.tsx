398d33c9e38b4c2ff3867a6d638a92a03ba7afa4
AvaN0x <clement.ricatte57@gmail.com>
2020-12-27 18:26:07 +0100

    added error managment for description

--- src/features/accounts/signup/Description.tsx
+++ src/features/accounts/signup/Description.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 
-import { TextBox, HiddenLabel } from '../../../components/styledComponents';
+import { TextBox, HiddenLabel, ErrorLabel } from '../../../components/styledComponents';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faExclamationTriangle, faUser } from "@fortawesome/free-solid-svg-icons";
 import { isNonNullChain } from "typescript";
 import { addDesc } from "../accountSlice";
 
 import IError from "../../../include/IError";
+import { useHistory } from "react-router-dom";
 
 export const Description = (): JSX.Element => {
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const [description, setDescription] = React.useState();
+    const [globalErrors, setGlobalErrors] = React.useState<Array<IError>>([]);
 
     const handleSetDescriptionOnChange = (event) => setDescription(event.target.value);
 
@@ -23,11 +26,26 @@ export const Description = (): JSX.Element => {
         if (!description || (description && description === isNonNullChain))
             errors = [...errors, { component: "description", label: "Veuillez spécifier votre description." } as IError];
 
+        setGlobalErrors(errors);
+        if (errors.length < 1) {
+            dispatch(addDesc(description));
+            history.push('/SignUp/4');
+        }
     };
 
     return (
         <form onSubmit={handleOnSubmit}>
-            <TextBox >
+            {globalErrors.length > 0 &&
+                <ErrorLabel>
+                    {globalErrors.map((error, index) => (
+                        <div key={index}>
+                            <FontAwesomeIcon icon={faExclamationTriangle} />
+                            {error.label}
+                        </div>
+                    ))}
+                </ErrorLabel>
+            }
+            <TextBox borderColor={globalErrors.some(e => e.component === "name") ? 'red' : 'default'}>
                 <FontAwesomeIcon icon={faUser} />
                 <input
                     type=''
