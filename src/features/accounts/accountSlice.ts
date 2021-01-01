238dff43a33b7edf2d776602d2948c4cd9624081
AvaN0x <clement.ricatte57@gmail.com>
2021-01-01 18:59:44 +0100

    add missing gender on slice

--- src/features/accounts/accountSlice.ts
+++ src/features/accounts/accountSlice.ts
@@ -358,6 +358,7 @@ export const getPrefsInfos = createSelector(
     getTags,
     (gender, orientation, tags) => {
         return {
+            gender,
             orientation,
             tags,
         };
