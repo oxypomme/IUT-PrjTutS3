ad9c3a26cbad45873d0fb699bb79b2a63c0128ce
OxyTom <oxytom8@gmail.com>
2021-01-09 16:34:37 +0100

    finished chat sagas/slice

--- src/app/sagas/messaging.ts
+++ src/app/sagas/messaging.ts
@@ -1,19 +1,70 @@
-import { call, put, takeLatest, select, take, all } from "redux-saga/effects";
+import { call, put, takeLatest, select, take, all, fork } from "redux-saga/effects";
 
 import { rsf } from "../firebase";
+import firebase from '@firebase/app';
 import "@firebase/database";
 
-function* fnc(action) {
-    try {
+import { getAuthId } from "../../features/accounts/accountSlice";
+import { fetchCurrProfilesSuccess } from "../../features/accounts/profileSlice";
+import { createMessageFailed, createMessageSuccess, newMessage, syncInMessagesSuccess, syncOutMessagesSuccess } from "../../features/chat/chatSlice";
 
-        yield "yolo";
+function* createMessage(action) {
+    try {
+        const authId = yield select(getAuthId);
+        if (authId == "") {
+            throw new Error("User not connected");
+        }
+
+        const { request } = action.payload;
+        const { data, ...params } = request.params;
+        yield call(
+            rsf.database[request.type],
+            request.url,
+            {
+                sender: authId,
+                ...data,
+                ...params
+            }
+        );
+
+        yield put(createMessageSuccess());
     } catch (error) {
-        yield "erreur";
+        yield put(createMessageFailed(error.message));
+    }
+}
+
+function* syncMessages() {
+    try {
+        yield take(fetchCurrProfilesSuccess);
+        const authId = yield select(getAuthId);
+
+        // Out messages
+        yield fork(
+            rsf.database.sync,
+            firebase.database().ref('messages').orderByChild('/sender').equalTo(authId),
+            {
+                successActionCreator: syncOutMessagesSuccess,
+            },
+            'value'
+        );
+
+        // In messages
+        yield fork(
+            rsf.database.sync,
+            firebase.database().ref('messages').orderByChild('/target').equalTo(authId),
+            {
+                successActionCreator: syncInMessagesSuccess,
+            },
+            'value'
+        );
+    } catch (error) {
+        yield error;
     }
 }
 
 export default function* chatSagas() {
     yield all([
-        takeLatest("type", fnc),
+        takeLatest(newMessage.type, createMessage),
+        fork(syncMessages),
     ]);
 }
--- src/features/chat/chatSlice.ts
+++ src/features/chat/chatSlice.ts
@@ -1,20 +1,76 @@
 import { createAction, createSelector, createSlice } from "@reduxjs/toolkit";
 
+import IMessage from "../../include/IMessage";
+
+export const newMessage = createAction(
+    "CREATE_MESSAGE_REQUESTED",
+    (message: IMessage, params = {}) => ({
+        payload: {
+            request: {
+                type: "create",
+                url: "/messages",
+                params: {
+                    data: message,
+                    ...params
+                }
+            }
+        }
+    })
+);
+
 export const chatSlice = createSlice({
     name: "chat",
     initialState: {
+        inMessages: {},
+        outMessages: {},
         isWorking: false,
         error: ""
     },
     extraReducers: {
-
+        [newMessage.type]: (state) => ({
+            ...state,
+            isWorking: true,
+            error: ""
+        })
     },
     reducers: {
-
+        syncInMessagesSuccess: (state, { payload: messages }) => ({
+            ...state,
+            isWorking: false,
+            error: "",
+            inMessages: messages
+        }),
+        syncOutMessagesSuccess: (state, { payload: messages }) => ({
+            ...state,
+            isWorking: false,
+            error: "",
+            outMessages: messages
+        }),
+        syncMessagesFailed: (state, { payload: error }) => ({
+            ...state,
+            isWorking: false,
+            error
+        }),
+        createMessageSuccess: state => ({
+            ...state,
+            isWorking: false,
+            error: ""
+        }),
+        createMessageFailed: (state, { payload: error }) => ({
+            ...state,
+            isWorking: false,
+            error
+        }),
     }
 });
 
-export const a = chatSlice.actions;
+export const {
+    syncInMessagesSuccess,
+    syncOutMessagesSuccess,
+    syncMessagesFailed,
+    createMessageSuccess,
+    createMessageFailed
+} = chatSlice.actions;
 
 export const getState = state => state.chat;
 
