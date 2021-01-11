1f00bd5d15ff000fd65d74b842e21396c0ec8068
AvaN0x <clement.ricatte57@gmail.com>
2021-01-11 14:24:08 +0100

    remove todo for checkbox

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -9,39 +9,9 @@ export const CheckBoxContainer = styled.p`
     font-size: .8rem;
     color: #525252;
 
-    // TODO fix broken css
     &>input[type=checkbox] {
         vertical-align: middle;
-        /* display: none; */
     }
-
-    /* &>input[type=checkbox] + label {
-        height: 12px;
-        width: 12px;
-        border: white solid 2px;
-        background-color: #00000000;
-        border-radius: 2px;
-        position: relative;
-        display: inline-block;
-        top: 2px;
-        cursor: pointer;
-    }
-
-    &>input[type=checkbox] + label::before {
-        content: '';
-        position: relative;
-        top: -5px;
-        line-height: 12px;
-    }
-
-    &>input[type=checkbox]:checked + label{
-        background-color: #ffffff;
-        color: #000000;
-    }
-
-    &>input[type=checkbox]:checked + label::before {
-        content: '✔';
-    } */
 `;
 
 
@@ -53,7 +23,6 @@ const CheckBox = ({ onChange, content }: any): JSX.Element => {
         setChecked(event.target.checked)
 
         onChange(event);
-        //? if the style need to be changed with script, do it here
     }
 
     return (
@@ -63,7 +32,6 @@ const CheckBox = ({ onChange, content }: any): JSX.Element => {
                 name="checkbox"
                 onChange={handleOnChange}
             />
-            {/* <label htmlFor="checkbox"></label> */}
             <span>{content || ""}</span>
         </CheckBoxContainer>
     );
